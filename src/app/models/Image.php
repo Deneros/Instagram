43fbf466a819b7485093ba1022028fac54cfaf84
Deneros <nicolas.velez99@hotmail.com>
2022-12-04 21:53:22 -0500

    [feat]: Implementacion de interface para la clase image

--- src/app/models/Image.php
+++ src/app/models/Image.php
@@ -1,8 +1,9 @@
 <?php namespace App\Models;
 
+use App\Interface\IPost;
 use App\Models\Post;
 
-class Image extends Post {
+class Image extends Post implements IPost{
     
     public function __construct(private string $message, private string $image)
     {
