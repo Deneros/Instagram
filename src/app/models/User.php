ea67275bb9e6b9c39ca9b6d7332722ae403d4953
Deneros <nicolas.velez99@hotmail.com>
2022-12-04 21:52:52 -0500

    [feat]: Se agregan nuevos metodos para el uso de la clase usuario

--- src/app/models/User.php
+++ src/app/models/User.php
@@ -22,6 +22,11 @@ class User
         return $this->id;
     }
 
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
     public function publish(Post $post): Post
     {
         array_push($this->posts, $post);
@@ -38,10 +43,39 @@ class User
         return $this->followers;
     }
 
-    public function showPosts()
+    public function showPosts(): void
     {
         foreach ($this->posts as $post) {
             var_dump($post->toString());
         }
+    }
+
+    private function hasFollower(User $user): int
+    {
+        $found = array_filter($this->followers, fn (User $follower) => $follower->id == $user->id);
+
+        return count($found) === 1;
+    }
+
+    public function addFollower(USer $user): void
+    {
+        if ($this->hasFollower($user)) {
+            if ($this->id == $user->id) {
+                print("No te puedes agregar a ti mismo como follower");
+            } else {
+                array_push($this->followers, $user);
+            }
+        } else {
+            print("El usuario ya es un follower");
+        }
+    }
+
+    public static function showProfile(User $user): string
+    {
+        $profile = "Nombre: $user->getUsername \n";
+        $profile .= "Followers: ". count($user->followers) . "\n";
+        $profile .= "Posts: ". count($user->posts). "\n";
+
+        return $profile;
     }
 }
